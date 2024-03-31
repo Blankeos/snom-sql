320b8a529c429b9cb76fc16dc8c4cefcaa541482
Blankeos <carloantonioct@gmail.com>
2024-03-31 15:42:36 +0800

    chore: Upgrading svelte to sveltekit and svelte 5

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,7 +1,23 @@
-import sveltePreprocess from 'svelte-preprocess'
+import adapter from "@sveltejs/adapter-static";
+// import sveltePreprocess from "svelte-preprocess";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
-}
+  preprocess: vitePreprocess(),
+
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
+    alias: {
+      // this will match src
+      "@": "src",
+    },
+  },
+};
+
+export default config;
